8e13106e6ccec68af6862e89aa5b1c3cf21ef810
Rifat Nabi <to.rifat@gmail.com>
2020-02-28 20:33:27 +1100

    Remove unnecessary less

--- npms.plugin.zsh
+++ npms.plugin.zsh
@@ -1,15 +1,14 @@
 function npms() {
   if [ ! -f package.json ]; then
     echo "package.json not found" >&2
-    return 1
-  fi
-
-  local command=$(jq '.scripts | keys[]' package.json -r | tr -d '"' | 
+  else 
+    local command=$(jq '.scripts | keys[]' package.json -r | tr -d '"' | 
     fzf --reverse \
       --preview-window=:wrap \
-      --preview "less package.json | jq '.scripts.\"{}\"' | tr -d '\"' | sed 's/^[[:blank:]]*//'")
+      --preview "jq '.scripts.\"{}\"' package.json -r | tr -d '\"' | sed 's/^[[:blank:]]*//'")
 
-  if [ -n "$command" ]; then
-    eval "npm run $command"
+    if [ -n "$command" ]; then
+      eval "npm run $command"
+    fi
   fi
 }
